60ba57d2f00b02775bc3fa086941235ccca0b74d
NoxYeah <luke.tomasik@gmail.com>
2021-02-27 20:00:37 -0600

    ADD: Pseduo code for understanding

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ let dom = {
   grid: document.querySelector('.grid'),
 }
 
-let choice
-
+// object that will replace false with TRUE once the BLOCK is selected
 
 let squaresTaken = {
   '1': false,
@@ -35,53 +34,112 @@ let squaresTaken = {
   '9': false
 }
 
+// object that will represent which player is active/whos turn is it
+
 let players = {
   one: false,
   two: false
 }
 
-let random = Math.trunc(Math.random()*2)
+
+// id's of block values that got selected will be pushed into the correspodning array
+// IE = if its playersOne turn it will push into playerOne array, playerTwo will push into thier own array
+// we will loop through array after EACH click to check for win conditions
 
 let playerSquaresOne = []
 let playerSquarestwo = []
 
+// activate the board/game
 dom.startGame.addEventListener('click', boardState)
 
- function boardState() {
- dom.grid.classList.remove('hide')
- dom.playerIcon[random].classList.add('active')
- players.random = true
- 
+// this function starts the game and will create a random "first Player"
+function boardState() {
+  dom.grid.classList.remove('hide')
+  goesFirst()
 }
 
+
+// will randomizer a player and will also set that player as 'true' meaning active
+function goesFirst() {
+  let random = Math.trunc(Math.random()*2)
+  dom.playerIcon[random].classList.add('active')
+  if (random === 0) {
+    players.one = true
+  } else {
+    players.two = true
+  }
+  console.log(players)
+  
+}
+
+// handles anytime a person clicks on the block values(grid)
 dom.grid.addEventListener('click', boardStateBubbling)
 
 
-
+// function that runs each time a tile/block value is clicked and pushes to ONLY playerOne for now
+// will defintely be cleaned up and will need to be reworked to make the ACTIVE player push into
+// thier corresponding array
 function boardStateBubbling(e) {
+  // this checks which player is active
   togglePlayer()
+  // this below will need to be redone to allow active player to push into corresponding array
+  // will need a function that checks if that square is availble
   playerSquaresOne.push(+e.target.id)
+  // this checks which player is an X or O and changes the innerHTML accordingly
   e.target.innerHTML = choice
+  // this will change the tiles that have been taken from false to true
   squaresTaken[e.target.id] = true
-// console.log(squaresTaken)
-//  console.log(playerSquaresOne)
-//  console.dir(e.target)
+
 }
 
+// allow us to assign X or O
+let choice
+
+// will toggle between the players that are active(whos turn is it) and will change the other player
+// to then be active
 function togglePlayer() {
-  // listens for an event
-if( players.one === true) {
+  // if playerone is active - do something
+  if( players.one === true) {
+    // changes playerOne to not active 
   players.one = false
+  // changes the second player to active
   players.two = true
+  // player one is X - this can be changed
   choice= 'X'
-  //do something place an X or O
-console.log('testing one')
-} else if (players.two ===true) {
+  
+} // if the second player is active
+else if (players.two ===true) {
+  // change the fire player to active
   players.one = true
+  // change the second player to non-active
   players.two = false
+  // assigns second player O - this can also be changed
   choice = "O"
-  console.log('testing Two')
+ 
 }
+// this will change the CSS active class once a player has selected a tile
+// this will need to be put into logic that checks to see if a square is able to be selected - 
+// IE if the square is alrdy selected it is still thier turn so we dont want to execute this function
+addActiveClass()
+}
+
+
+// displays the CSS active player icon
+
+function addActiveClass() {
+  // checks if player one is active
+  if (players.one === true) {
+    // will remove active from player one
+    dom.playerIcon[0].classList.add('active')
+    // will add active to player Two
+    dom.playerIcon[1].classList.remove('active')
+  } else {
+    
+    // will add active to player Two
+    dom.playerIcon[1].classList.add('active')
+    // will remove active from player one
+    dom.playerIcon[0].classList.remove('active')
+  }
 }
 
 
@@ -89,8 +147,7 @@ console.log('testing one')
 
 
 
-
-
+// yup.... did not touch - will need help on tieing these classes in
 
 // dom.playerIcon[0].classList.add('active')
 class Board {
