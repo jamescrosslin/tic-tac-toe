dfe8dc1f7a2f2aa203a54d6ddb2370bcd7724074
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-05 15:53:00 -0500

    Removed start game button on start changed inner text to new game, messed with styling a tiny tiny bit

--- index.js
+++ index.js
@@ -18,24 +18,36 @@ function addActiveClass() {
 }
 
 
-let game;
+let game= null;
+
 dom.startGame.addEventListener("click", () => {
+  dom.startGame.classList.add('hide')
   game = new Game(dom.playerIcon, dom.grid);
   game.startGame();
   dom.grid.classList.remove("hide");
   addActiveClass();
+  
+  
 });
 
 dom.grid.addEventListener("click", (e) => {
   if (e.target.tagName === "DIV" && !e.target.classList.contains("selected") && game) {
     e.target.innerHTML = game.activePlayer.symbol;
     boardStateBubbling(e);
-    if(game.handleSelection(e.target)) return game = "" ;
     addActiveClass();
-
-
+    if(game.handleSelection(e.target)){
+      dom.startGame.classList.remove('hide');
+      dom.startGame.innerText = 'New Game'
+      return game = null} 
+      if(dom.startGame.innerText==='New Game'){
+        console.log('ok')
+      }
+     
   }
 });
 
+if(dom.startGame.innerText==='New Game'){
+  console.log('ok')
+}
 
 
