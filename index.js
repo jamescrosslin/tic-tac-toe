797fba65c78ff924384817adb12c15781bf80ee4
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-05 16:37:46 -0500

    Trying to make dom reflect player symbol

--- index.js
+++ index.js
@@ -11,38 +11,43 @@ function boardStateBubbling(e) {
 
 
 function addActiveClass() {
+  
   for (const player of [...game.playerElements]) {
     player.classList.remove("active");
   }
   game.activePlayer.playerElement.classList.add("active");
+  
 }
 
 
-let game= null;
+let game;
 
 dom.startGame.addEventListener("click", () => {
   dom.startGame.classList.add('hide')
   game = new Game(dom.playerIcon, dom.grid);
   game.startGame();
-  dom.grid.classList.remove("hide");
   addActiveClass();
-  
+  dom.grid.classList.remove("hide");
+  // document.getElementById(`${game.player1.symbol}`).innerText = game.player1.symbol;
+  // document.getElementById(`${game.player2.symbol}`).innerText = game.player2.symbol;
+ 
   
 });
 
 dom.grid.addEventListener("click", (e) => {
   if (e.target.tagName === "DIV" && !e.target.classList.contains("selected") && game) {
     e.target.innerHTML = game.activePlayer.symbol;
-    boardStateBubbling(e);
-    addActiveClass();
-    if(game.handleSelection(e.target)){
-      dom.startGame.classList.remove('hide');
-      dom.startGame.innerText = 'New Game'
-      return game = null} 
+        boardStateBubbling(e);    
+
+      if(game.handleSelection(e.target)){
+        dom.startGame.classList.remove('hide');
+        dom.startGame.innerText = 'New Game'
+        return game = null} 
+  
+  addActiveClass();
      
      
-     
-  }
+    } 
 });
 
 
